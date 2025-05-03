fefd9f31b90e0faa4870fb981feebd02f847da97
Patryk Miler <patryk.miler@student.pk.edu.pl>
2025-05-03 16:04:59 +0200

    feat(react_app): update ComponentCard to include interaction buttons and adjust layout

--- react_app/src/ComponentPages/components/ComponentCard.jsx
+++ react_app/src/ComponentPages/components/ComponentCard.jsx
@@ -3,16 +3,24 @@ import ComponentHeader from "./ComponentHeader.jsx";
 import ComponentDetails from "./ComponentDetails.jsx";
 import ComponentPreview from "./ComponentPreview.jsx";
 import {Link} from "react-router-dom";
+import ButtonCopy from "./ButtonCopy.jsx";
+import ButtonLike from "./ButtonLike.jsx";
+import ButtonShare from "./ButtonShare.jsx";
 
 
 const ComponentCard = ({component}) => (
-    <div className={styles.listItem} key={component.id}>
-        <Link to={`/components/${component.id}`} className={styles.listItemInside}>
-            <ComponentHeader component={component}/>
-            <ComponentDetails component={component}/>
-            <ComponentPreview component={component}/>
-        </Link>
-    </div>
+        <div className={styles.listItem} key={component.id}>
+            <Link to={`/components/${component.id}`} className={styles.listItemInside}>
+                <ComponentPreview component={component}/>
+                <ComponentHeader component={component}/>
+                <ComponentDetails component={component}/>
+            </Link>
+            <div className={styles.interactionButtonsContainerList}>
+                <ButtonCopy component={component}/>
+                <ButtonShare component={component}/>
+                <ButtonLike component={component}/>
+            </div>
+        </div>
 );
 
 export default ComponentCard
