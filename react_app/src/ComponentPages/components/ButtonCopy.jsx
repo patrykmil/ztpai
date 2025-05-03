d96fc4d1567eb14623a77e4df544b7bb87d48869
Patryk Miler <patryk.miler@student.pk.edu.pl>
2025-05-03 14:56:36 +0200

    feat(react_app): add copy and share buttons for component interaction

---
+++ react_app/src/ComponentPages/components/ButtonCopy.jsx
@@ -0,0 +1,20 @@
+import styles from "./Component.module.css";
+
+const ButtonCopy = ({component}) => {
+    const copy = async () => {
+        const string = `//${component.name} by ${component.author.name}\n//iu.iu/components/${component.id}\n\n//----HTML----\n${component.html}\n\n//----CSS----\n${component.css}\n`;
+        try {
+            await navigator.clipboard.writeText(string);
+        }catch (e) {
+            console.error('Failed to copy: ', e);
+        }
+    };
+
+    return (
+        <button className={styles.interactionButton} onClick={() => copy(component)}>
+            <img src={"/icons/copy.svg"} alt={"Copy"}/>
+        </button>
+    );
+};
+
+export default ButtonCopy;
---
+++ react_app/src/ComponentPages/components/ButtonShare.jsx
@@ -0,0 +1,20 @@
+import styles from "./Component.module.css";
+
+const ButtonShare = ({component}) => {
+    const copyLink = async () => {
+        const string = `iu.iu/components/${component.id}`;
+        try {
+            await navigator.clipboard.writeText(string);
+        }catch (e) {
+            console.error('Failed to copy: ', e);
+        }
+    };
+
+    return (
+        <button className={styles.interactionButton} onClick={() => copyLink(component)}>
+            <img src={"/icons/share.svg"} alt={"Copy link"}/>
+        </button>
+    );
+};
+
+export default ButtonShare;
