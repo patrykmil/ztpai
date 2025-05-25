dec579f5cf5045083b71c34a7a1071e0281f50c2
Patryk Miler <patryk.miler@student.pk.edu.pl>
2025-05-25 16:19:36 +0200

    fix(spring_app): fix updating set in modifyComponent

--- spring_app/src/main/java/iu/iu/spring_app/api/components/service/ModifyComponentService.java
+++ spring_app/src/main/java/iu/iu/spring_app/api/components/service/ModifyComponentService.java
@@ -4,6 +4,8 @@ import iu.iu.spring_app.api.components.model.Component;
 import iu.iu.spring_app.api.components.model.Set;
 import iu.iu.spring_app.api.components.model.Type;
 import iu.iu.spring_app.api.components.repository.ComponentRepository;
+import iu.iu.spring_app.api.users.model.User;
+import iu.iu.spring_app.api.users.service.GetUserService;
 import jakarta.transaction.Transactional;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
@@ -18,12 +20,13 @@ public class ModifyComponentService {
     private final ValidationService validationService;
     private final TypeService typeService;
     private final SetService setService;
+    private final GetUserService getUserService;
 
     public ModifyComponentService(ComponentRepository componentRepository,
                                   GetComponentService getComponentService,
                                   TagsService tagsService,
                                   ColorService colorService,
-                                  ValidationService validationService, TypeService typeService, SetService setService) {
+                                  ValidationService validationService, TypeService typeService, SetService setService, GetUserService getUserService) {
         this.componentRepository = componentRepository;
         this.getComponentService = getComponentService;
         this.tagsService = tagsService;
@@ -31,17 +34,20 @@ public class ModifyComponentService {
         this.validationService = validationService;
         this.typeService = typeService;
         this.setService = setService;
+        this.getUserService = getUserService;
     }
 
     @Transactional
     public Component modifyComponent(Component payload, String email) {
         validationService.validateComponent(payload);
 
+        User user = getUserService.getUserByEmail(email);
+
         Component oldComponent = getComponentService.getComponentById(payload.getId());
 
         Type type = typeService.getTypeByName(validationService.sanitizeInput(payload.getType().getName()));
 
-        Set set = setService.getSetByName(validationService.sanitizeInput(payload.getSet().getName()), payload.getAuthor());
+        Set set = setService.getSetByName(validationService.sanitizeInput(payload.getSet().getName()), user);
 
         if(!set.getUser().getEmail().equals(email)) {
             throw new AccessDeniedException("Not allowed to use this set");
